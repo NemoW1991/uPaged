5da7f76cc96ed5f6438044d72b00c7d505a4f4c4
Summer Huang <hforest1991@gmail.com>
2021-10-31 19:12:52 +1100

    build(types): for shift detail data type

---
+++ frontend/src/lib/types/index.ts
@@ -0,0 +1,14 @@
+export interface Shift {
+  id: string,
+  dateOfDay: string,
+  date: string,
+  startTime: string,
+  endTime: string,
+  careType: string,
+  salary: number,
+  hospital: string,
+  starRate: number,
+  street: string,
+  state: string,
+  isInvited: boolean
+}
