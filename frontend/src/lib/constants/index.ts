252011d4da47c9c32c8fb1ad3a044686ac52e6d9
Summer Huang <hforest1991@gmail.com>
2021-10-31 14:25:07 +1100

    add lib: constants for navbar data

---
+++ frontend/src/lib/constants/index.ts
@@ -0,0 +1,14 @@
+export const navbarData = [
+  {
+    title: 'Find Work',
+    link: '/job',
+  },
+  {
+    title: 'Timesheets',
+    link: '/timesheets',
+  },
+  {
+    title: 'Messaging',
+    link: '/messaging',
+  }
+]
