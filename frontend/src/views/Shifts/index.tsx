e4c7df40c7d5c91594ffe1e73f7ca46f91f23502
Summer Huang <hforest1991@gmail.com>
2021-10-31 19:24:47 +1100

    feat(Shifts): Shifts page

--- frontend/src/views/Shifts/index.tsx
+++ frontend/src/views/Shifts/index.tsx
@@ -1,10 +1,42 @@
 import React from 'react';
+import Shift from '../../components/Shift';
+import Button from '../../components/Button';
+import {Shifts as ShiftsData} from '../../lib/constants';
+import DateSelect from '../../components/DateSelect';
+import './index.scss';
 
 const Shifts: React.FC = () => {
   return (
-    <div>
-      Shifts
-    </div>
+    <section className='shifts'>
+      <h1>Shifts</h1>
+      <h2>you've been invited</h2>
+
+      {/* Invited Shifts Item */}
+      {ShiftsData.map((shift)=>
+        <div className='shifts__item'>
+          <div className='shifts__item-side shifts__item--green'></div>
+          <Shift key={shift.id} shift={shift} />
+          <div className='shifts__item-console'>
+            <Button buttonText='decline' hollow={true} />
+            <Button buttonText='apply' />
+          </div>
+        </div>
+      )}
+
+      <DateSelect />
+      
+      {/* Shifts Item */}
+      {ShiftsData.map((shift)=>
+        <div className='shifts__item'>
+          <div className='shifts__item-side shifts__item--blue'></div>
+          <Shift key={shift.id} shift={shift} />
+          <div className='shifts__item-console'>
+            <Button buttonText='apply' />
+          </div>
+        </div>
+      )}
+      
+    </section>
   )
 }
 
