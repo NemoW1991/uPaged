56110fdd3b6d7f3c33a692ea5a0911bcf5e35611
Summer Huang <hforest1991@gmail.com>
2021-11-01 09:31:42 +1100

    update: for integration with BE

--- frontend/src/views/Shifts/index.tsx
+++ frontend/src/views/Shifts/index.tsx
@@ -1,19 +1,51 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {AppState} from '../../store';
+import { getShifts } from '../../store/effects/Shift';
 import Shift from '../../components/Shift';
 import Button from '../../components/Button';
-import {Shifts as ShiftsData} from '../../lib/constants';
+import {Shift as ShiftType} from '../../lib/types';
 import DateSelect from '../../components/DateSelect';
 import './index.scss';
 
 const Shifts: React.FC = () => {
+
+  const {count} = useSelector((state: AppState) => state.count)
+  const {
+    start,
+    shiftsOneInvited, 
+    shiftsOneUninvited,
+    shiftsTwoInvited,
+    shiftsTwoUninvited
+  } = useSelector((state: AppState) => state.shifts);
+  const dispatch = useDispatch();
+  const [invitedShifts, setInvitedShifts] = useState<ShiftType[]>([]);
+  const [uninvitedShifts, setUninvitedShifts] = useState<ShiftType[]>([]);
+
+  useEffect(() => {
+    dispatch(getShifts())
+  }, [dispatch]);
+
+  useEffect(() => {
+    console.log(start)
+    if(count === 0) {
+      setInvitedShifts(shiftsOneInvited);
+      setUninvitedShifts(shiftsOneUninvited)
+    } else {
+      setInvitedShifts(shiftsTwoInvited);
+      setUninvitedShifts(shiftsTwoUninvited)
+    }
+  }, [count,shiftsOneInvited,shiftsOneUninvited,shiftsTwoInvited,shiftsTwoUninvited]);
+
+
   return (
     <section className='shifts'>
       <h1>Shifts</h1>
       <h2>you've been invited</h2>
 
       {/* Invited Shifts Item */}
-      {ShiftsData.map((shift)=>
-        <div className='shifts__item'>
+      {invitedShifts.map((shift)=>
+        <div className='shifts__item' key={shift.id}>
           <div className='shifts__item-side shifts__item--green'></div>
           <Shift key={shift.id} shift={shift} />
           <div className='shifts__item-console'>
@@ -26,8 +58,8 @@ const Shifts: React.FC = () => {
       <DateSelect />
       
       {/* Shifts Item */}
-      {ShiftsData.map((shift)=>
-        <div className='shifts__item'>
+      {uninvitedShifts.map((shift)=>
+        <div className='shifts__item' key={shift.id}>
           <div className='shifts__item-side shifts__item--blue'></div>
           <Shift key={shift.id} shift={shift} />
           <div className='shifts__item-console'>
