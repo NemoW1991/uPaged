5aa9dacd37a01fbce83daeb51f7f30ce472b382d
Summer Huang <hforest1991@gmail.com>
2021-11-01 09:29:20 +1100

    feat(DateSelect): DateSelect component

--- frontend/src/components/DateSelect/index.tsx
+++ frontend/src/components/DateSelect/index.tsx
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {AppState} from '../../store';
+import { getShifts } from '../../store/effects/Shift';
+import {setCountAction} from '../../store/actions/CountAction';
+import {getYear, getLastMonth, getDate, getlastDate} from '../../lib/util';
 import './index.scss';
 
 const DateSelect: React.FC = () => {
-  const handleClick = (direction: string) => {
+  const {count} = useSelector((state: AppState) => state.count);
+  const {start, end} = useSelector((state: AppState) => state.shifts);
+  const dispatch = useDispatch();
+  const [startDate, setStartDate] = useState<string>('');
 
+  useEffect(() => {
+    if(count === 0) {
+      setStartDate(start);
+      const date = getlastDate(start);
+    }  else {
+      setStartDate(end);
+    }   
+  }, [count,start,end]);
+
+  const handleClick = (direction: number) => {
+    
+    let index = count + direction;
+    if(index>1 || index<0) {
+      direction > 0 ? dispatch(getShifts(end, direction)) : dispatch(getShifts(start, direction)) 
+    } else {
+      dispatch(setCountAction(index))
+    }
   };
   return (
     <div className='date-select'>
-      <button className='button' onClick={() => handleClick('backward')}> &lt; </button>
-      <span>21</span>
+      <button className='button' onClick={() => handleClick(-1)}> &lt; </button>
+      <span>{getDate(startDate)>10?getDate(startDate):'0'+getDate(startDate)}</span>
       <span>-</span>
-      <span>28</span>
-      <span>Apr</span>
-      <span>2019</span>
-      <button className='button' onClick={() => handleClick('forward')}>&gt;</button>
+      <span>{getlastDate(startDate)}</span>
+      <span>{getLastMonth(startDate)}</span>
+      <span>{getYear(startDate)}</span>
+      <button className='button' onClick={() => handleClick(1)}>&gt;</button>
     </div>
   )
 }
